b326e57c59bdea73eebd238f291ceae02400744e
Juneunbae <dmsqo1403@naver.com>
2025-01-18 14:47:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/clonecatchtablebackend/CloneCatchTableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.clonecatchtablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCatchTableBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneCatchTableBackendApplication.class, args);
+    }
+
+}
